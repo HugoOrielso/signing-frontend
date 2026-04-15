c372ea2f4a55c2ba88e75492a885b443294202e9
Hugo Orielso <orielso.lozano15@gmail.com>
2026-04-15 01:19:11 -0500

    changed NEXT_API_URL BY API_URL

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api";
+const API_URL = process.env.API_URL  || "http://localhost:4000/api";
 
 const nextConfig: NextConfig = {
   output: "standalone",
