850ee9f050fbd4a8b9f627d0b19a850b09a6943f
Hugo Orielso <orielso.lozano15@gmail.com>
2026-05-16 06:27:42 -0500

    probando next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const API_URL = process.env.API_URL || "http://localhost:4000/api";
+console.log(API_URL)
 
 const nextConfig: NextConfig = {
   output: "standalone",
